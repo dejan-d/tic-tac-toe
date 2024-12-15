392e5320adae95ea7f9ab16866a854782e455df8
Best Father in the World <apple@DABA-2.local>
2024-12-15 16:42:04 +0100

    first committ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick(){
+    setValue('X');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button
+      className="square" 
+      onClick={handleClick}>
+        {value}
+      </button>
+  );
+}
+
+function Board(){
+  return(
+  <div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  </div>
+  );
+}
+
+function Game(){
+  return(
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Game;
